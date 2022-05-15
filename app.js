61a702b108e1886c71eb9bf5e7322d1e8b11930d
Libor Vachal <libor.vachal@gmail.com>
2022-05-15 13:10:28 +0200

    Initialize repo by express generator

---
+++ app.js
@@ -0,0 +1,46 @@
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from './routes/index.js'
+import weatherRouter from './routes/weather.js'
+
+const PORT = 3000
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/weather', weatherRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`)
+})

9dd8a57ccad931ade63084189168bf65d4958681
Libor Vachal <libor.vachal@gmail.com>
2022-06-19 19:53:55 +0200

    feat: initalize terraform deployment

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "lipelix"
+
+    workspaces {
+      name = "weather-for-humans"
+    }
+  }
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "5.0.2"
+    }
+  }
+}
+
+provider "heroku" {
+  email   = "lipelix@gmail.com"
+  api_key = var.heroku_api_key
+}
+
+resource "heroku_app" "this" {
+  name   = "pocasi-pro-lidi"
+  region = "eu"
+
+  config_vars = {
+    # FOOBAR = "baz"
+  }
+
+  buildpacks = [
+    "heroku/nodejs"
+  ]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "heroku_api_key" {
+  type      = string
+  sensitive = true
+}

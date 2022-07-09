ab87292f4d472df7eb1dbc3bb2d25d9feaef718e
Libor Vachal <libor.vachal@gmail.com>
2022-07-09 17:59:23 +0200

    fix: it sucks

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,7 +65,7 @@ resource "null_resource" "this" {
   }
 
   provisioner "local-exec" {
-    command = "curl ${local.github_release} | openssl sha256 | awk '{print \"SHA256:\"$1}' >> ${path.module}/latest_release_sha256.txt"
+    command = "curl ${local.github_release} | openssl sha256 | awk \"{print 'SHA256:'$1}\" >> ${path.module}/latest_release_sha256.txt"
   }
 }
 

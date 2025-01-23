20aeb985381a612c96e10242d16d6d6400e01c72
Libor Váchal <libor.vachal@heureka.cz>
2025-01-23 21:59:37 +0100

    feat: initialize cloud run deployment

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,11 +12,20 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "5.0.0-rc1"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
   }
 }
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = "us-east1" // Free Tier is only available in us-east1, us-west1, and us-central1 regions
 }
 
 output "Cloudflare_account" {

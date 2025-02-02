d0a8c7cfd0df61ffa43187ae7719f6a1bbc8ee52
Libor Váchal <libor.vachal@heureka.cz>
2025-02-02 20:58:10 +0100

    feat: expose custom domain in app engine routing

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ export const CONFIG = (() => {
   const PORT = process.env.PORT || 3000;
   const PORT_HTTPS = 3001;
 
-  const hostname = process.env.NODE_ENV === 'development' ? `localhost:${PORT_HTTPS}` : process.env.HOSTNAME;
+  const hostname = process.env.NODE_ENV === 'development' ? `localhost:${PORT_HTTPS}` : process.env.HTTP_HOST;
 
   console.log('env: ', {
     PORT,
-    HOSTNAME: hostname
+    HTTP_HOST: hostname
   });
 
   return {
-    HOSTNAME: hostname,
+    HTTP_HOST: hostname,
     ROUTES: {
       getWeather: `https://${hostname}/weather`,
       getWeatherByIP: `https://${hostname}/weather/by-ip`

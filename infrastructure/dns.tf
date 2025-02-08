9f24577be7401cc5ec0ecc28a849873e1be1375c
Libor Váchal <libor.vachal@heureka.cz>
2025-02-08 14:59:16 +0100

    feat: add gcp app engine ips to cloudflare

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -6,13 +6,33 @@ resource "cloudflare_zone" "pocasi_pro_lidi_cz" {
   type = "full"
 }
 
-resource "cloudflare_dns_record" "pocasi_pro_lidi_all" {
-  zone_id = cloudflare_zone.pocasi_pro_lidi_cz.id
-  name    = "pocasi-pro-lidi.cz"
-  content = "139.59.142.55"
-  type    = "A"
-  proxied = true
-  ttl     = 1
+resource "cloudflare_dns_record" "pocasi_pro_lidi_records_ipv4" {
+  for_each = toset(["216.239.32.21", "216.239.34.21", "216.239.36.21", "216.239.38.21"])
+  zone_id  = cloudflare_zone.pocasi_pro_lidi_cz.id
+  name     = "@"
+  content  = each.key
+  type     = "A"
+  proxied  = true
+  ttl      = 1
+}
+
+resource "cloudflare_dns_record" "pocasi_pro_lidi_records_ipv6" {
+  for_each = toset(["2001:4860:4802:32::15", "2001:4860:4802:34::15", "2001:4860:4802:36::15", "2001:4860:4802:38::15"])
+  zone_id  = cloudflare_zone.pocasi_pro_lidi_cz.id
+  name     = "@"
+  content  = each.key
+  type     = "AAAA"
+  proxied  = true
+  ttl      = 1
+}
+
+resource "cloudflare_dns_record" "pocasi_pro_lidi_records_cname" {
+  zone_id  = cloudflare_zone.pocasi_pro_lidi_cz.id
+  name     = "www"
+  content  = "ghs.googlehosted.com"
+  type     = "CNAME"
+  proxied  = true
+  ttl      = 1
 }
 
 output "Cloudlflare_zone_metadata" {
